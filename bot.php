58066d74e967144375ba51f1a9c78b6e86202b2e
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-12-08 01:12:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text."test"
+				'text' => "https://marketdata.set.or.th/mkt/realtimestockchart?symbol=".$text."&type=AccValue"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
