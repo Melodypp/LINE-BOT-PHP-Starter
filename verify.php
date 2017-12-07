c114d05131f51ac95f9eea36f014e684205e1c7b
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-12-08 00:36:04 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:Yac5n1y3qAiNglC';
 $access_token = 'oShK7OOuNFHR7etOIUcQyEMUZ6xHxS/LmH2QM5XfMJhEzVK4OISus0t33ZCRwRo2RQjlxaIJ1nQWXUSHcabSB1Bnc7HPwAoim2vKBLyGWZFtizH1PiEzRqgm5Z4r1XsJAFgqwnIbFkelunGE+E2a4gdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +11,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
